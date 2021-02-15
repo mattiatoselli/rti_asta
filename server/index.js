7ea5f14ed9754daddda5676481567551c17dac6d
Giacomo Corallini <corallinigiacomo89@gmail.com>
2021-02-15 18:47:56 +0100

    npm script

--- server/index.js
+++ server/index.js
@@ -11,14 +11,16 @@ app.use(cors());
 //routes
 const drivers = require("./routes/api/drivers");
 const transactions = require("./routes/api/transactions");
+
 //redirect any routes from api/drivers to file routes/api/drivers
 app.use("/api/drivers", drivers);
+
 //redirect any routes from api/transactions to file routes/api/transactions
 app.use("/api/transactions", transactions);
 
 
 // open server on port 3000
-const port = 3000;
+const port = 4000;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-    });
+});
--- server/routes/api/drivers.js
+++ server/routes/api/drivers.js
@@ -4,57 +4,60 @@ const router = express.Router();
 var ObjectId = require('mongodb').ObjectId;
 
 //list drivers
-router.get("/", async (req,res)=>{
+router.get("/", async (req, res) => {
     const drivers = await loadDriversCollection();
     var sort = req.query.sort;
     var order = parseInt(req.query.order);
     var skip = parseInt(req.query.skip);
     var limit = parseInt(req.query.limit);
-    var mysort = { };
+    var mysort = {};
     mysort[sort] = order;
     res.send(await drivers.find({}).sort(mysort).skip(skip).limit(limit).toArray());
 });
 
 //select single driver
-router.get("/:id", async (req,res)=>{
+router.get("/:id", async (req, res) => {
     const drivers = await loadDriversCollection();
-    var selectedDriver = await drivers.findOne({_id:ObjectId(req.params.id)});
+    var selectedDriver = await drivers.findOne({ _id: ObjectId(req.params.id) });
     res.status(200).send(selectedDriver);
 });
+
 //create driver
-router.post("/", async(req, res)=>{
+router.post("/", async (req, res) => {
     const drivers = await loadDriversCollection();
     await drivers.insertOne({
-        name : req.body.name,
+        name: req.body.name,
         team: req.body.team,
-        price : req.body.price,
-        isOnSale : req.body.isOnSale
+        price: parseInt(req.body.price),
+        isOnSale: req.body.isOnSale
     });
     res.status(201).send();
 });
+
 //update driver
-router.patch("/:id", async(req,res)=>{
+router.patch("/:id", async (req, res) => {
     const drivers = await loadDriversCollection();
     var newDriverData = {
-        name : req.body.name,
+        name: req.body.name,
         team: req.body.team,
-        price : req.body.price,
-        isOnSale : req.body.isOnSale
+        price: req.body.price,
+        isOnSale: req.body.isOnSale
     };
-    await drivers.updateOne({_id: ObjectId(req.params.id)}, { $set: newDriverData });
+    await drivers.updateOne({ _id: ObjectId(req.params.id) }, { $set: newDriverData });
     res.status(200).send();
 });
+
 //delete driver
-router.delete("/:id", async(req,res)=>{
+router.delete("/:id", async (req, res) => {
     const drivers = await loadDriversCollection();
-    await drivers.deleteOne({_id:ObjectId(req.params.id)});
+    await drivers.deleteOne({ _id: ObjectId(req.params.id) });
     return res.status(204).send();
 });
 
 async function loadDriversCollection() {
     const client = await mongodb.MongoClient.connect("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
-    {useNewUrlParser: true},
-    {useUnifiedTopology: true}
+        { useNewUrlParser: true },
+        { useUnifiedTopology: true }
     );
     return client.db("racing_team_italia").collection("drivers");
 }
